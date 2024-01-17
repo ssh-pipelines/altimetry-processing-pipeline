a55a5867324d97f5b2b857c64507dd35f52cccc4
marlis <kevin.marlis@jpl.nasa.gov>
2024-01-16 16:57:26 -0800

    Added collection metadata class for handling global metadata unification across all source datasets

---
+++ daily_files/collection_metadata.py
@@ -0,0 +1,37 @@
+from typing import Iterable
+
+
+class CollectionMeta:
+    source: str
+    source_url: str
+    reference: str
+
+    def __init__(self, source, source_url, reference) -> None:
+        self.source = source
+        self.source_url = source_url
+        self.reference = reference
+
+
+class AllCollections:
+    collections: Iterable[CollectionMeta] = {
+        "C2619443998-POCLOUD": CollectionMeta(
+            "Sentinel-6A MF Jason-CS L2 P4 Altimeter Low Resolution (LR) NTC Reduced Ocean Surface Topography F08",
+            "https://podaac.jpl.nasa.gov/dataset/JASON_CS_S6A_L2_ALT_LR_RED_OST_NTC_F08",
+            "https://doi.org/10.5067/S6AP4-2LRNTR-F08",
+        ),
+        "C2619444006-POCLOUD": CollectionMeta(
+            "Sentinel-6A MF Jason-CS L2 P4 Altimeter Low Resolution (LR) NTC Reduced Ocean Surface Topography (Unvalidated) F08",
+            "https://podaac.jpl.nasa.gov/dataset/JASON_CS_S6A_L2_ALT_LR_RED_OST_NTC_F08_UNVALIDATED",
+            "https://doi.org/10.5067/S6AP4-2LRNUR-F08",
+        ),
+        "C1968980609-POCLOUD": CollectionMeta(
+            "Sentinel-6A MF Jason-CS L2 P4 Altimeter Low Resolution (LR) STC Reduced Ocean Surface Topography",
+            "https://podaac.jpl.nasa.gov/dataset/JASON_CS_S6A_L2_ALT_LR_RED_OST_STC_F",
+            "https://doi.org/10.5067/S6AP4-2LRST",
+        ),
+        "C2204129664-POCLOUD": CollectionMeta(
+            "Integrated Multi-Mission Ocean Altimeter Data for Climate Research Version 5.1",
+            "https://podaac.jpl.nasa.gov/dataset/MERGED_TP_J1_OSTM_OST_CYCLES_V51",
+            "https://doi.org/10.5067/ALTUG-TJ151",
+        ),
+    }

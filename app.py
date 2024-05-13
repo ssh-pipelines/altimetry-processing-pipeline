67fde7e0f59781d14e38395fb2aabcaec2e2733b
marlis <kevin.marlis@jpl.nasa.gov>
2024-05-13 15:38:10 -0700

    Added comments for handling two methods of invocation

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import json
 from daily_files import daily_file_job
 
 def handler(event, context):
+    # Handle messages from SQS
     if 'Records' in event:
         for record in event['Records']:
             message_body = json.loads(record['body'])
             daily_file_job.start_job(message_body)
+    # Handle direct Lambda invocations
     else:
         daily_file_job.start_job(event)

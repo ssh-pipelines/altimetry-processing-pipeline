bb4605502e17e164869d093057e87f3ed132c2e1
kevinmarlis <kevinmarlis@gmail.com>
2026-02-13 15:08:56 -0800

    Updates to reflect changes in s3 path construction

--- pipeline/other_products/enso/app.py
+++ pipeline/other_products/enso/app.py
@@ -6,13 +6,14 @@ from enso_jobs import enso_processing
 def handler(event, context):
     bucket = event.get("bucket")
     date = event.get("date")
+    source = event.get("source")
 
-    if None in [bucket, date]:
-        raise ValueError("One of date, or bucket job parameters missing.")
+    if None in [bucket, date, source]:
+        raise ValueError("One of date, source, or bucket job parameters missing.")
 
     try:
         date = datetime.fromisoformat(date)
-        enso_processing.start_job(date, bucket)
+        enso_processing.start_job(date, bucket, source)
     except Exception as e:
         error_response = {
             "status": "error",
